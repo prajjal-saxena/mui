3ebd08b95efdfa8716fdf012bba8d6322f2679e5
prajjal-saxena <66246890+prajjal-saxena@users.noreply.github.com>
2023-09-15 16:49:28 +0530

    Update Buttonui.js

--- Buttonui.js
+++ Buttonui.js
@@ -1,8 +1,12 @@
-import { Button, ButtonGroup, Stack } from "@mui/material"
+import { Button, ButtonGroup, Stack, ToggleButtonGroup, ToggleButton } from "@mui/material"
 import React from "react";
 import SendIcon from '@mui/icons-material/Send';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { IconButton } from '@mui/material';
+import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+
 
 const Buttonui = () => {
   return (
@@ -29,11 +33,20 @@ const Buttonui = () => {
           </IconButton>
       </Stack>
       
-      <ButtonGroup size="small" color="secondary" variant="contained">
-        <Button>Left</Button>
+      <ButtonGroup size="small" color="secondary" variant="contained" orientation="vertical"
+      aria-label="alignment button group">
+        <Button onClick={()=> alert('Left Click')}>Left</Button>
         <Button>Center</Button>
         <Button>Right</Button>
       </ButtonGroup>
+
+      <Stack direction='row'>
+          <ToggleButtonGroup aria-label="text-formatting">
+             <ToggleButton value='bold' aria-label='bold'><FormatBoldIcon /></ToggleButton>
+             <ToggleButton value='italic' aria-label='italic'><FormatItalicIcon/></ToggleButton>
+             <ToggleButton value='underlined' aria-label='underlined'><FormatUnderlinedIcon/></ToggleButton>
+          </ToggleButtonGroup>
+      </Stack>
     </>
   )
 }
